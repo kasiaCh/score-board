550736467f98ff776b20fcd0153a89a6a1487a18
katarzyna.cholewa <katarzyna.cholewa@unitygroup.com>
2025-06-13 13:54:32 +0200

    ref: some refactor

--- src/main/java/com/example/scoreboard/service/ScoreBoardService.java
+++ src/main/java/com/example/scoreboard/service/ScoreBoardService.java
@@ -4,6 +4,7 @@ import com.example.scoreboard.domain.Game;
 import com.example.scoreboard.domain.GameRepository;
 import com.example.scoreboard.infrastructure.InMemoryGameRepository;
 import com.example.scoreboard.service.exception.InValidDataException;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.Comparator;
 import java.util.List;
@@ -17,30 +18,21 @@ public class ScoreBoardService {
         gameRepository = new InMemoryGameRepository(); // could be injected when using Spring
     }
 
-    public void startGame(String homeTeam, String awayTeam) {
-        if (gameRepository.exists(homeTeam, awayTeam)) {
-            throw new InValidDataException("Game between " + homeTeam + " and " + awayTeam + " already exists.");
-        }
+    public void startGame(@NotBlank String homeTeam, @NotBlank String awayTeam) {
+        validateGameNotExistYet(homeTeam, awayTeam);
 
         gameRepository.save(new Game(homeTeam, awayTeam));
     }
 
-    public void finishGame(String homeTeam, String awayTeam) {
-        if (!gameRepository.exists(homeTeam, awayTeam)) {
-            throw new InValidDataException("Game between " + homeTeam + " and " + awayTeam + " does not exist.");
-        }
+    public void finishGame(@NotBlank String homeTeam, @NotBlank String awayTeam) {
+        validateGameExists(homeTeam, awayTeam);
 
         gameRepository.delete(homeTeam, awayTeam);
     }
 
-    public void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore) {
-        if (!gameRepository.exists(homeTeam, awayTeam)) {
-            throw new InValidDataException("Game between " + homeTeam + " and " + awayTeam + " does not exist.");
-        }
-
-        if (homeScore < 0 || awayScore < 0) {
-            throw new InValidDataException("Scores must be non-negative.");
-        }
+    public void updateScore(@NotBlank String homeTeam, @NotBlank String awayTeam, int homeScore, int awayScore) {
+        validateGameExists(homeTeam, awayTeam);
+        validateScore(homeScore, awayScore);
 
         gameRepository.updateScore(homeTeam, awayTeam, homeScore, awayScore);
     }
@@ -51,6 +43,24 @@ public class ScoreBoardService {
                 .sorted(Comparator.comparingInt(Game::totalScore).reversed()
                         .thenComparing(games::indexOf, Comparator.reverseOrder()))
                 .collect(Collectors.toList());
+    }
+
+    private void validateGameNotExistYet(String homeTeam, String awayTeam) {
+        if (gameRepository.exists(homeTeam, awayTeam)) {
+            throw new InValidDataException("Game between " + homeTeam + " and " + awayTeam + " already exists.");
+        }
+    }
+
+    private void validateGameExists(String homeTeam, String awayTeam) {
+        if (!gameRepository.exists(homeTeam, awayTeam)) {
+            throw new InValidDataException("Game between " + homeTeam + " and " + awayTeam + " does not exist.");
+        }
+    }
+
+    private static void validateScore(int homeScore, int awayScore) {
+        if (homeScore < 0 || awayScore < 0) {
+            throw new InValidDataException("Scores must be non-negative.");
+        }
     }
 
 }
